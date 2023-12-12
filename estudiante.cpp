0b9f69735358046f64971077869387420fd0e92f
NlodeL <nlodelop@myuax.com>
2023-12-12 12:20:05 +0100

    creo la estructura y explico los valores que se le añaden

---
+++ estudiante.cpp
@@ -0,0 +1,8 @@
+#include "estudiante.h"
+ //Aqui empezaremos a crear la estrucutura estudiante
+ struct Estudiante {
+     char nombre[50];
+     int edad;
+     float promedio;
+ };
+// Definimos los seiguientes datos de la estrucura, nombre, edad y promedio
