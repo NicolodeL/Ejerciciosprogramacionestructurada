6fdd68e6b51605c0a45da078565ae8967b1cc234
NlodeL <nlodelop@myuax.com>
2023-12-13 10:11:54 +0100

    añado el operador malloc y defino la estrucutra

--- estudiante.cpp
+++ estudiante.cpp
@@ -1,7 +1,8 @@
-#include "estudiante.h"
+
  //Aqui empezaremos a crear la estrucutura estudiante
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // a. Definición de la estructura Estudiante
 struct Estudiante {
@@ -11,8 +12,20 @@ struct Estudiante {
 };
 
 // e. Funciones para añadir, ver y eliminar estudiantes
-void agregarEstudiante(struct Estudiante lista[], int *numEstudiantes);
-void verEstudiantes(struct Estudiante lista[], int numEstudiantes);
-void eliminarEstudiante(struct Estudiante lista[], int *numEstudiantes);
+void instanciacionYAsignacion() {
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
 
 // Definimos los siguientes datos de la estrucura, nombre, edad y promedio en el main.cpp
+
+     struct Estudiante *estudiante2 = (struct Estudiante *)malloc(sizeof(struct Estudiante));
+     if (estudiante2 == NULL) {
+         printf("Error al asignar memoria con malloc.\n");
+         return;
+     }
+
+     strcpy(estudiante2->nombre, "Maria");
+     estudiante2->edad = 22;
+     estudiante2->promedio = 7.8;
