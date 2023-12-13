411aa5c4ce077968733f3c6fed9d073ff1efec05
NlodeL <nlodelop@myuax.com>
2023-12-13 12:09:38 +0100

    empiezo a definir la estructura de copia de la estrucutra estudiante en el main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+struct Estudiante copiarEstudiante(const struct Estudiante *original) {
+    struct Estudiante copia;
+
+    // Copiar cada miembro de la estructura original a la copia
+    strcpy(copia.nombre, original->nombre);
+    copia.edad = original->edad;
+    copia.promedio = original->promedio;
+
+    return copia;
+}
+
+
 int main() {
     // b. Instanciación de la estructura Estudiante y asignación de valores
     struct Estudiante estudiante1;
